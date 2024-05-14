0014159a27249a6f317f91fad2f44464012f1d1f
alpha-gr <grande.alfry@gmail.com>
2024-05-15 00:11:37 +0200

    put themes in themes.js

--- App.js
+++ App.js
@@ -8,7 +8,8 @@ import CurrentWeatherCard from './components/CurrentWeatherCard.js';
 import styles from './styles.js';
 import { StrictMode, useState } from 'react';
 import { IconButton, Surface, Text } from 'react-native-paper';
-import { PaperProvider, Appbar, Button, ActivityIndicator, MD3LightTheme, MD3DarkTheme} from 'react-native-paper';
+import { PaperProvider, Appbar, Button, ActivityIndicator} from 'react-native-paper';
+import { darkTheme, lightTheme } from './themes.js';
 import Search from './components/Search'
 import { getWeatherData } from './components/weather.js';
 import Forecast from './components/Forecast.js';
@@ -20,101 +21,6 @@ import { useCalendars } from 'expo-localization';
 //TODO: display errors to user
 //TODO: clean up the state variables ( use a reducer ?)
 //TODO: too many rerenders, optimize
-
-const lightTheme =
-{
-  ...MD3LightTheme,
-  "colors": {
-    "primary": "rgb(0, 105, 107)",
-    "onPrimary": "rgb(255, 255, 255)",
-    "primaryContainer": "rgb(111, 246, 249)",
-    "onPrimaryContainer": "rgb(0, 32, 33)",
-    "secondary": "rgb(74, 99, 99)",
-    "onSecondary": "rgb(255, 255, 255)",
-    "secondaryContainer": "rgb(204, 232, 232)",
-    "onSecondaryContainer": "rgb(4, 31, 32)",
-    "tertiary": "rgb(76, 95, 124)",
-    "onTertiary": "rgb(255, 255, 255)",
-    "tertiaryContainer": "rgb(212, 227, 255)",
-    "onTertiaryContainer": "rgb(6, 28, 54)",
-    "error": "rgb(186, 26, 26)",
-    "onError": "rgb(255, 255, 255)",
-    "errorContainer": "rgb(255, 218, 214)",
-    "onErrorContainer": "rgb(65, 0, 2)",
-    "background": "rgb(250, 253, 252)",
-    "onBackground": "rgb(25, 28, 28)",
-    "surface": "rgb(250, 253, 252)",
-    "onSurface": "rgb(25, 28, 28)",
-    "surfaceVariant": "rgb(218, 228, 228)",
-    "onSurfaceVariant": "rgb(63, 73, 73)",
-    "outline": "rgb(111, 121, 121)",
-    "outlineVariant": "rgb(190, 200, 200)",
-    "shadow": "rgb(0, 0, 0)",
-    "scrim": "rgb(0, 0, 0)",
-    "inverseSurface": "rgb(45, 49, 49)",
-    "inverseOnSurface": "rgb(239, 241, 241)",
-    "inversePrimary": "rgb(76, 218, 221)",
-    "elevation": {
-      "level0": "transparent",
-      "level1": "rgb(238, 246, 245)",
-      "level2": "rgb(230, 241, 240)",
-      "level3": "rgb(223, 237, 236)",
-      "level4": "rgb(220, 235, 235)",
-      "level5": "rgb(215, 232, 232)"
-    },
-    "surfaceDisabled": "rgba(25, 28, 28, 0.12)",
-    "onSurfaceDisabled": "rgba(25, 28, 28, 0.38)",
-    "backdrop": "rgba(41, 50, 50, 0.4)"
-  }
-}
-
-
-const darkTheme = 
-{
-  ...MD3DarkTheme,
-  "colors": {
-    "primary": "rgb(76, 218, 221)",
-    "onPrimary": "rgb(0, 55, 56)",
-    "primaryContainer": "rgb(0, 79, 81)",
-    "onPrimaryContainer": "rgb(111, 246, 249)",
-    "secondary": "rgb(176, 204, 204)",
-    "onSecondary": "rgb(27, 52, 53)",
-    "secondaryContainer": "rgb(50, 75, 76)",
-    "onSecondaryContainer": "rgb(204, 232, 232)",
-    "tertiary": "rgb(180, 200, 233)",
-    "onTertiary": "rgb(30, 49, 76)",
-    "tertiaryContainer": "rgb(53, 72, 99)",
-    "onTertiaryContainer": "rgb(212, 227, 255)",
-    "error": "rgb(255, 180, 171)",
-    "onError": "rgb(105, 0, 5)",
-    "errorContainer": "rgb(147, 0, 10)",
-    "onErrorContainer": "rgb(255, 180, 171)",
-    "background": "rgb(25, 28, 28)",
-    "onBackground": "rgb(224, 227, 226)",
-    "surface": "rgb(25, 28, 28)",
-    "onSurface": "rgb(224, 227, 226)",
-    "surfaceVariant": "rgb(63, 73, 73)",
-    "onSurfaceVariant": "rgb(190, 200, 200)",
-    "outline": "rgb(137, 147, 146)",
-    "outlineVariant": "rgb(63, 73, 73)",
-    "shadow": "rgb(0, 0, 0)",
-    "scrim": "rgb(0, 0, 0)",
-    "inverseSurface": "rgb(224, 227, 226)",
-    "inverseOnSurface": "rgb(45, 49, 49)",
-    "inversePrimary": "rgb(0, 105, 107)",
-    "elevation": {
-      "level0": "transparent",
-      "level1": "rgb(28, 38, 38)",
-      "level2": "rgb(29, 43, 43)",
-      "level3": "rgb(31, 49, 49)",
-      "level4": "rgb(31, 51, 51)",
-      "level5": "rgb(32, 55, 55)"
-    },
-    "surfaceDisabled": "rgba(224, 227, 226, 0.12)",
-    "onSurfaceDisabled": "rgba(224, 227, 226, 0.38)",
-    "backdrop": "rgba(41, 50, 50, 0.4)"
-  }
-}
 
 export default function App() {
 
