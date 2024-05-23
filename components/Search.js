51227c8539133fe853e2df6ef034ea96e4acfbb1
alpha-gr <grande.alfry@gmail.com>
2024-05-23 23:14:59 +0200

    city search now supports different languages

--- components/Search.js
+++ components/Search.js
@@ -5,10 +5,11 @@ descriptions = require('./WeatherDescriptions.js') //import the descriptions
 import { useState, useEffect } from 'react'
 import CountryFlag from 'react-native-country-flag';
 import styles from '../styles.js';
+import { useLocales } from 'expo-localization';
 const max_fetch_results = 5
 
 export default function Search(props){
-    const [ city, setCity ] = useState('')
+        const [ city, setCity ] = useState('') 
     const [ placeholder, setPlaceholder ] = useState('enter city name')
     return (
         <>
@@ -27,8 +28,9 @@ export default function Search(props){
 }
 
 function CityList(props){
-
+    const calendar = useLocales()[0]
     city= props.city
+    languageCode = calendar.languageCode
     const [cityList, setCityList] = useState([])
     displayList = []
     emptyCity = props.emptyCity
@@ -40,7 +42,7 @@ function CityList(props){
             return
         }
 
-        fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=${max_fetch_results}&language=en&format=json`)
+        fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=${max_fetch_results}&language=${languageCode}&format=json`)
         .then(response => response.json())
         .then(data => {
             if(!ignore){
@@ -57,10 +59,19 @@ function CityList(props){
     }
         
     displayList = cityList.map((item) => {
+        if(item.country_code==undefined ){
+            if(item.name == 'Antarctica'){
+                item.country_code = 'AQ' //the endpoint does not return the country code for Antarctica
+            }
+            else{   
+                item.country_code = 'UN' //in case the endpoint does not return the country code
+            }
+        }
+        description = [item.country, item.admin1].filter(Boolean).join(', ')
         return <List.Item
             key={item.id}
             title={item.name}
-            description={(item.country!=undefined?item.country:'') + (item.admin1!=undefined ? ', '+item.admin1 : '')}
+            description={description}
             left={props => <CountryFlag isoCode={item.country_code} size={20}/>}
             onPress={() => {props.onClick(item);  emptyCity(item.name) ; setCityList([]);}}
         />
