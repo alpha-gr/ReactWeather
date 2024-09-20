73a37c06cf8697a12eda9ba9932014eaa2470091
alpha-gr <grande.alfry@gmail.com>
2024-09-20 11:46:59 +0200

    test for animated icons

--- components/WeatherIcon.js
+++ components/WeatherIcon.js
@@ -1,26 +1,12 @@
-//this component displays an image of a weather icon
-//the image is determined by the weather code and the time of day
-//the time of day is determined by the isDay property of the weather data
-//the weather code is a WMO code
-//the weather icons can be found in the assets/icons folder
-//the icons can be STATIC or ANIMATED
+import { createIconSet } from '@expo/vector-icons';
+import icoMoonConfig from './selection.json';
+const Icon = createIconSet(icoMoonConfig, 'icomoon', 'icomoon.ttf');
 
-import React from "react";
-import { Image } from "react-native";
-import PropTypes from "prop-types";
-import { animated_icons } from "./WMOanimated_icons";
-import { static_icons } from "./WMOstatic_icons";
-import Icon from '../assets/icons/animated_icons/day.svg'
+export default Icon;
 
-export default function WeatherIcon(props){
-  const code = props.code;
-  const isDay = props.isDay;
-  const size = props.size;
-  const style = props.style;
-  const iconSize = size === "large" ? 100 : 70;
-  const icons = style === "static" ? static_icons : animated_icons;
-  const icon = isDay ? icons.find(icon => icon.code === code).day : icons.find(icon => icon.code === code).night;
+export const Button = Icon.Button;
+export const TabBarItem = Icon.TabBarItem;
+export const TabBarItemIOS = Icon.TabBarItemIOS;
+export const ToolbarAndroid = Icon.ToolbarAndroid;
+export const getImageSource = Icon.getImageSource;
 
-  //return <Image source={icon} style={{ width: iconSize, height: iconSize }} />;
-  return <Icon width={iconSize} height={iconSize} />;
-}
