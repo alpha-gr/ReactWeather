de4fb75cd9e73b57909c085ae1a9609950f11ab2
alpha-gr <grande.alfry@gmail.com>
2024-10-29 17:37:38 +0100

    now only one geolocation query can be done at a time

--- App.js
+++ App.js
@@ -69,6 +69,9 @@ export default function App() {
   }, [city]);
 
   const geoLocationHandlePress = async () => {
+    if(isLoading){
+      return
+    }
     console.log("geoLocationHandlePress called");
     setIsLoading(true);
 
