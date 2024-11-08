d0325195a3f13cd8aece086d5f8890fefb7d8ff3
alpha-gr <grande.alfry@gmail.com>
2024-11-08 19:18:36 +0100

    refactored the async function to be cleaner

--- components/geoLocation.js
+++ components/geoLocation.js
@@ -1,38 +1,33 @@
 import * as Location from "expo-location";
 
-export default fetchGeoLocation = async () => {
-  return new Promise((resolve, reject) => {
-    Location.requestForegroundPermissionsAsync()
-      .then((status) => {
-        if (!status.granted) {
-          reject("Location permission denied");
-          return; // Exit early if permission is denied
-        }
-        return Location.getCurrentPositionAsync({});
-      })
-      .then(location => {
-        if (location) {
-          const coords = location.coords;
-          // Now chain the reverse geocoding fetch
-          return fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${coords.latitude}&longitude=${coords.longitude}&localityLanguage=en`);
-        }
-      })
-      .then(response => response.json())
-      .then(data => {
-        console.log("Received inverse geocoding data");
-        
-        let city = {};
-        city.name = data["city"];
-        city.latitude = data["latitude"];
-        city.longitude = data["longitude"];
-        city.admin1 = data["principalSubdivision"];
-        city.isCurrentLocation = true;
+const fetchGeoLocation = async () => {
+  try {
+    const status = await Location.requestForegroundPermissionsAsync();
+    if (!status.granted) {
+      throw new Error("Location permission denied");
+    }
 
-        resolve(city); // Resolve with city data
-      })
-      .catch(error => {
-        console.error("Error in the geolocation or reverse geocoding process:", error);
-        reject("Error in geolocation or reverse geocoding");
-      });
-  });
+    const location = await Location.getCurrentPositionAsync({});
+    const { latitude, longitude } = location.coords;
+
+    const response = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`);
+    const data = await response.json();
+
+    console.log("Received inverse geocoding data");
+
+    const city = {
+      name: data.city,
+      latitude: data.latitude,
+      longitude: data.longitude,
+      admin1: data.principalSubdivision,
+      isCurrentLocation: true,
+    };
+
+    return city;
+  } catch (error) {
+    console.error("Error in the geolocation or reverse geocoding process:", error);
+    throw new Error("Error in geolocation or reverse geocoding");
+  }
 };
+
+export default fetchGeoLocation;
